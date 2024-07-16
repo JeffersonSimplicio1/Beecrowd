a80e77c78320b40e42ad28bfe9b8d6fa65c0d3a1
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-15 23:31:00 -0300

    Soluções exercicios 1000 e 1001

---
+++ Main/src/Main_1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main_1001 {
+
+    public static void main(String[] args) throws Exception{
+        Scanner leitor = new Scanner(System.in);
+
+            int a = leitor.nextInt();
+            int b = leitor.nextInt();
+            int x = a+b;
+
+        System.out.println("X = " + x);
+    }
+}
