09bd4e39b3aef5529f8f294c253d4ec8eec2075d
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-18 22:27:13 -0300

    alguns ajustes no codigo 1001

--- Main/src/Main_1001.java
+++ Main/src/Main_1001.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
-import java.io.IOException;
 
 public class Main_1001 {
 
     public static void main(String[] args) throws Exception{
-        Scanner leitor = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            int a = leitor.nextInt();
-            int b = leitor.nextInt();
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
             int x = a+b;
 
         System.out.println("X = " + x);
