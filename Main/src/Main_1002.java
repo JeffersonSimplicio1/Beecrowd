24f1a444c068b052e0e6856f55435b0678ebfc64
Smalls <jefferson.a.simplicio@gmail.com>
2024-07-18 22:25:07 -0300

    Mais uma lição do beecrowd

---
+++ Main/src/Main_1002.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main_1002 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+       int a = scanner.nextInt();
+       int b = scanner.nextInt();
+        int soma = a+b;
+        
+    System.out.println("SOMA = " + soma);
+    
+    
+    
+    
+    
+    }
+    
+}
